1fe73e01814c45100853221256bdc054ba6b678e
James Ives <iam@jamesiv.es>
2017-09-02 18:26:48 -0500

    Class Name Clarification

--- src/components/credits.js
+++ src/components/credits.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+export default class Credits extends Component {
   render() {
     return (
       <div className="credits-container">
